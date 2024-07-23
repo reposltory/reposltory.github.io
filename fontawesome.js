edc26c06c5a7b82a409181232c9251bdb9f75dfd
reposltory <124707435+reposltory@users.noreply.github.com>
2024-07-24 02:36:20 +0330

    Create fontawesome.js

---
+++ fontawesome.js
@@ -0,0 +1,6 @@
+var noticeList = document.getElementById('_notice_list').firstChild.childElementCount;
+    
+    if (!noticeList) {
+        console.warn("Element with id '_notice_list' not found.");
+    }
+    console.log("Total number of children of children:", noticeList);
