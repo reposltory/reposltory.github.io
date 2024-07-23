2d1e86e32f922b4fbac5dadb7330600cbf8d8ba7
reposltory <124707435+reposltory@users.noreply.github.com>
2024-07-24 02:38:44 +0330

    Update fontawesome.js

--- fontawesome.js
+++ fontawesome.js
@@ -1,6 +1,10 @@
-var noticeList = document.getElementById('_notice_list').firstChild.childElementCount;
+document.addEventListener('DOMContentLoaded', function() {
+    var noticeList = document.getElementById('_notice_list').firstChild.childElementCount;
     
     if (!noticeList) {
         console.warn("Element with id '_notice_list' not found.");
+        return;
     }
     console.log("Total number of children of children:", noticeList);
+    
+});
